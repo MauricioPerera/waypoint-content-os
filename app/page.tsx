e35c0750e6263359eaa8d5cdc7483bc7234b5e6b
Mauricio Perera <mauricio.perera@gmail.com>
2026-08-30 15:44:20 -0600

    feat: add human template administration

--- app/page.tsx
+++ app/page.tsx
@@ -1071,7 +1071,7 @@ export default function Home() {
               }}
             />
           )}
-          {view === "pages" && <VisualCanvas pages={pages} templates={templates} layouts={layouts} createPage={createPage} updatePage={updatePage} removePage={removePage} createTemplate={createTemplate} removeTemplate={removeTemplate} updateLayout={updateLayout} />}
+          {view === "pages" && <VisualCanvas pages={pages} templates={templates} layouts={layouts} createPage={createPage} updatePage={updatePage} removePage={removePage} createTemplate={createTemplate} removeTemplate={removeTemplate} updateLayout={updateLayout} updateTemplate={updateTemplate} />}
           {view === "plugins" && (
             <Plugins
               plugins={plugins}
